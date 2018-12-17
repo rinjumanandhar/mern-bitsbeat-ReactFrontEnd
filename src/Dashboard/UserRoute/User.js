c6735b9d984a6f937a1e057ca2dc7455b55ec926
sudrishya sen <sudrishyasen34@gmail.com>
2018-12-17 13:01:14 +0545

    added new things

--- src/Dashboard/UserRoute/User.js
+++ src/Dashboard/UserRoute/User.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Input, Menu } from 'semantic-ui-react'
+import { Input, Menu, Icon } from 'semantic-ui-react'
 
 class User extends Component {
   state = { activeItem: 'role' }
@@ -12,7 +12,7 @@ class User extends Component {
     return (
       <Menu size='large' vertical>
       <Menu.Item>
-          <Input icon='search' placeholder='Search...' />
+        <Input icon={<Icon name='search' inverted circular link /> }placeholder='Search...' />
         </Menu.Item>
         <Menu.Item name='role' active={activeItem === 'role'} onClick={this.handleItemClick}>
           Role
