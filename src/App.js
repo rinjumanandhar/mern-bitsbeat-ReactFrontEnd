fae542fa284a5c5a29881c886810d1f33237c8b5
sudrishya sen <sudrishyasen34@gmail.com>
2018-12-17 13:27:00 +0545

    edited the file app.js

--- src/App.js
+++ src/App.js
@@ -6,27 +6,10 @@ import MainRoute from './Dashboard/Route';
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      searchText: '',
-      searchResult: [],
-      contactList: []
-    }
-  }
   render() {
     return (
       <div className="row">
-        <div className="col-xs-12 col-sm-offset-3 col-sm-6">
-          <span className="title">Contacts</span>
-          <SearchBar />
-          <ul cl className="list-group" id ="contact-list">
-            <li className="list-group-item">
-            <ContactCard />
-            </li>
-          </ul>
-        </div>
-          <MainRoute />
+        <MainRoute/>
        </div>
     );
   }
